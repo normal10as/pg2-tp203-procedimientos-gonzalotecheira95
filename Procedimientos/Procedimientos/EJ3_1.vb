b17911774ceba2d3fbe35055f1b983ac8e8b85bd
Gonzalo Techeira <gnxa95@hotmail.com>
2018-05-14 20:37:02 -0300

    ...

---
+++ Procedimientos/Procedimientos/EJ3_1.vb
@@ -0,0 +1,20 @@
+﻿Module EJ3_1
+
+    Private fecha As Date = Now
+
+    Sub Main()
+        Fechaactual()
+        Horaactual()
+
+        Console.ReadLine()
+    End Sub
+
+    Private Sub Fechaactual()
+        Console.WriteLine("Fecha Actual: " & fecha.Day & "/" & fecha.Month & "/" & fecha.Year)
+    End Sub
+
+    Private Function Horaactual()
+        Console.WriteLine("Hora Actual: " & fecha.Hour & ":" & fecha.Minute)
+    End Function
+
+End Module
