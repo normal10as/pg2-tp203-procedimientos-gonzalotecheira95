dd620a9436d8e6d50b1273ca216a1a637b3e0244
Gonzalo Techeira <gnxa95@hotmail.com>
2018-06-02 18:18:21 -0300

    cambios e.e

--- Procedimientos/Procedimientos/EJ3_7.vb
+++ Procedimientos/Procedimientos/EJ3_7.vb
@@ -1,45 +1,79 @@
-﻿Module EJ3_7
+﻿Enum Operaciones
+    suma = 1
+    resta
+    multiplicacion
+    division
+End Enum
+Module EJ3_7
 
     Sub Main()
 
-        parametros()
-
+        'Operacion con 2 valores
+        Console.WriteLine("Suma con 2 valores " & Calcular(Operaciones.suma, 2, 2))
+        Console.WriteLine("Resta con 2 valores " & Calcular(Operaciones.resta, 23, 10))
+        Console.WriteLine("Multiplicacion con 2 valores " & Calcular(Operaciones.multiplicacion, 5, 2))
+        Console.WriteLine("Division con 2 valores " & Calcular(Operaciones.division, 18, 2) & vbCrLf & vbCrLf)
+        'Operacion con 3 valores
+        Console.WriteLine("Suma con 3 valores " & Calcular(Operaciones.suma, 2, 2, 3))
+        Console.WriteLine("Resta con 3 valores " & Calcular(Operaciones.resta, 23, 10, 2))
+        Console.WriteLine("Multiplicacion con 3 valores " & Calcular(Operaciones.multiplicacion, 5, 2, 2))
+        Console.WriteLine("Division con 3 valores " & Calcular(Operaciones.division, 18, 2, 3) & vbCrLf & vbCrLf)
+        'Operacion con 4 valores
+        Console.WriteLine("Suma con 4 valores " & Calcular(Operaciones.suma, 2, 2, 2, 2))
+        Console.WriteLine("Resta con 4 valores " & Calcular(Operaciones.resta, 23, 10, 5, 20))
+        Console.WriteLine("Multiplicacion con 4 valores " & Calcular(Operaciones.multiplicacion, 5, 2, 3, 2))
+        Console.WriteLine("Division con 4 valores " & Calcular(Operaciones.division, 18, 2, 3, 2) & vbCrLf)
+        'suma
+        Console.WriteLine("Suma " & Calcular(2, 4))
         Console.ReadKey()
 
     End Sub
-    Private Sub parametros()
-        Dim x As Integer
-        Dim y As Integer
-        Dim ops As Integer
-        Dim r As Double
 
-        Console.WriteLine("Ingrese dos numerosa para realizar la operacion: ")
-        x = Console.ReadLine()
-        y = Console.ReadLine()
-        Console.WriteLine("1-Suma 2-Resta 3- Divison 4- Multiplicacion....: ")
-        ops = Console.ReadLine()
-        r = operaciones(x, y, ops)
+    Function Calcular(operaciones As Operaciones, ByRef a As Integer, ByRef b As Integer) As Double
 
-        Console.WriteLine("El resultado de la operacion es: " & r)
-
-
-
-    End Sub
-    Public Function operaciones(ByRef a As Integer, ByRef b As Integer, ByRef op As Double) As Double
-        Dim r As Double
-
-        Select Case op
-            Case 1
-                r = a + b
-            Case 2
-                r = a - b
-            Case 3
-                r = a / b
-            Case 4
-                r = a * b
+        Select Case operaciones
+            Case Operaciones.suma
+                Return a + b
+            Case Operaciones.resta
+                Return a - b
+            Case Operaciones.multiplicacion
+                Return a * b
+            Case Operaciones.division
+                Return a / b
         End Select
-
-        Return r
+        Return operaciones
     End Function
+    Function Calcular(operaciones As Operaciones, ByRef a As Integer, ByRef b As Integer, ByRef c As Integer) As Double
+
+        Select Case operaciones
+            Case Operaciones.suma
+                Return a + b + c
+            Case Operaciones.resta
+                Return a - b - c
+            Case Operaciones.multiplicacion
+                Return a * b * c
+            Case Operaciones.division
+                Return a / b / c
+        End Select
+        Return operaciones
+    End Function
+    Function Calcular(operaciones As Operaciones, ByRef a As Integer, ByRef b As Integer, ByRef c As Integer, ByRef d As Integer) As Double
+
+        Select Case operaciones
+            Case Operaciones.suma
+                Return a + b + c + d
+            Case Operaciones.resta
+                Return a - b - c - d
+            Case Operaciones.multiplicacion
+                Return a * b * c * d
+            Case Operaciones.division
+                Return a / b / c / d
+        End Select
+        Return operaciones
+    End Function
+    Function Calcular(ByRef a As Integer, ByRef b As Integer) As Integer
+        Return a + b
+    End Function
+
 
 End Module
---
+++ Procedimientos/Procedimientos/EJ3_8.vb
@@ -0,0 +1,86 @@
+﻿Enum Operaciones2
+    suma = 1
+    resta
+    multiplicacion
+    division
+End Enum
+Module EJ3_8
+    Sub Main()
+        Dim calculo = New Operaciones2
+
+        Resultado(calculo, 50, 50) 'con 2 valores
+        Resultado(calculo, 50, 5, 25) 'con 3 valores
+        Resultado(calculo, 50, 2, 2, 5) ' con 5 valores
+
+        'suma
+        Console.WriteLine("Suma " & Calcular(2, 4))
+        Console.ReadKey()
+
+    End Sub
+    Friend Sub Resultado(operaciones2 As Operaciones2, ByRef a As Integer, ByRef b As Integer)
+        'Operacion con 2 valores
+        Console.WriteLine("Suma con 2 valores " & Calcular(Operaciones2.suma, a, b))
+        Console.WriteLine("Resta con 2 valores " & Calcular(Operaciones2.resta, a, b))
+        Console.WriteLine("Multiplicacion con 2 valores " & Calcular(Operaciones2.multiplicacion, a, b))
+        Console.WriteLine("Division con 2 valores " & Calcular(Operaciones2.division, a, b) & vbCrLf & vbCrLf)
+    End Sub
+    Friend Sub Resultado(operaciones2 As Operaciones2, ByRef a As Integer, ByRef b As Integer, ByRef c As Integer)
+        'Operacion con 3 valores
+        Console.WriteLine("Suma con 3 valores " & Calcular(Operaciones2.suma, a, b, c))
+        Console.WriteLine("Resta con 3 valores " & Calcular(Operaciones2.resta, a, b, c))
+        Console.WriteLine("Multiplicacion con 3 valores " & Calcular(Operaciones2.multiplicacion, a, b, c))
+        Console.WriteLine("Division con 3 valores " & Calcular(Operaciones2.division, a, b, c) & vbCrLf & vbCrLf)
+    End Sub
+    Friend Sub Resultado(operaciones2 As Operaciones2, ByRef a As Integer, ByRef b As Integer, ByRef c As Integer, ByRef d As Integer)
+        'Operacion con 4 valores
+        Console.WriteLine("Suma con 4 valores " & Calcular(Operaciones2.suma, a, b, c, d))
+        Console.WriteLine("Resta con 4 valores " & Calcular(Operaciones2.resta, a, b, c, d))
+        Console.WriteLine("Multiplicacion con 4 valores " & Calcular(Operaciones2.multiplicacion, a, b, c, d))
+        Console.WriteLine("Division con 4 valores " & Calcular(Operaciones2.division, a, b, c, d) & vbCrLf)
+    End Sub
+    Function Calcular(operaciones2 As Operaciones2, ByRef a As Integer, ByRef b As Integer) As Double
+
+        Select Case operaciones2
+            Case Operaciones.suma
+                Return a + b
+            Case Operaciones.resta
+                Return a - b
+            Case Operaciones.multiplicacion
+                Return a * b
+            Case Operaciones.division
+                Return a / b
+        End Select
+        Return operaciones2
+    End Function
+    Function Calcular(operaciones2 As Operaciones, ByRef a As Integer, ByRef b As Integer, ByRef c As Integer) As Double
+
+        Select Case operaciones2
+            Case Operaciones.suma
+                Return a + b + c
+            Case Operaciones.resta
+                Return a - b - c
+            Case Operaciones.multiplicacion
+                Return a * b * c
+            Case Operaciones.division
+                Return a / b / c
+        End Select
+        Return operaciones2
+    End Function
+    Function Calcular(operaciones2 As Operaciones, ByRef a As Integer, ByRef b As Integer, ByRef c As Integer, ByRef d As Integer) As Double
+
+        Select Case operaciones2
+            Case Operaciones.suma
+                Return a + b + c + d
+            Case Operaciones.resta
+                Return a - b - c - d
+            Case Operaciones.multiplicacion
+                Return a * b * c * d
+            Case Operaciones.division
+                Return a / b / c / d
+        End Select
+        Return Operaciones2
+    End Function
+    Function Calcular(ByRef a As Integer, ByRef b As Integer) As Integer
+        Return a + b
+    End Function
+End Module
